7705f725a817098c611cc5efd783da9579a4b0b2
jayanthh71 <jayanth37069@gmail.com>
2025-05-20 20:51:39 +0400

    feat: add main menu

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello World!");
+  document
+    .getElementById("main-menu__button")
+    .addEventListener("click", showPlayerSelect);
 });
+
+function showPlayerSelect() {
+  const mainMenu = document.getElementById("main-menu");
+  const playerSelect = document.getElementById("player-select");
+
+  mainMenu.classList.add("hidden");
+  playerSelect.style.display = "";
+  void playerSelect.offsetWidth;
+
+  setTimeout(() => {
+    playerSelect.classList.remove("hidden");
+  }, 50);
+}
